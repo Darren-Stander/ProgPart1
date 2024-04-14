ff96fa23db055b2e640ca184fed1badb4dd72903
Darren-Stander <darrenjadestander@gmail.com>
2024-04-14 18:43:56 +0200

    <Over in line 24, is the start of the nested menu option that a user selects for a option>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,58 @@ namespace ProgPart1
         {
             bool close = false;
 
-            ingredients.EnterIngredients();
-            ingredinets.EnterSteps();
+            //ingredients.EnterIngredients();
+            //ingredinets.EnterSteps();
 
-            while(!close)
+            while (!close)
+            {
+                Console.WriteLine("Welcome to our app enter your....");
+
+
+                Console.WriteLine("\nOptions");
+                Console.WriteLine("a. View Recipe");
+                Console.WriteLine("b. Scale Recipe");
+                Console.WriteLine("b. Reset Recipe");
+                Console.WriteLine("c. Delete Recipe");
+                Console.WriteLine("d. Close Application");
+
+               
+                string choices;
+                if (!string.IsNullOrEmpty(Console.ReadLine()))
+                {
+                    | Console.WriteLine("Invalid opotion, enter a a,b,c or d");
+                    continue;
+                }
+
+                switch (close) 
+                {
+                    case a:
+                        recipe.displayrecipe();
+                        break;
+
+                    case b:
+                        recipe.scalerecipe;
+                        break;
+
+                    case c:
+                        recipe.resetrecipe;
+                        break;
+                    case c:
+                        recipe.deleterecipe;
+                        break;
+                    case d:
+                        close = true;
+                        break;
+                        default:
+                        Console.WriteLine("Invalid selection");
+                        continue;
+                        
+                }
+                if (!close)
+                {
+
+                }
+            }
         }
     }
 }
