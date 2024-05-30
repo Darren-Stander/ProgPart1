e6b70b8aa81b909d82395ab363f96de738a4eeed
Darren-Stander <darrenjadestander@gmail.com>
2024-05-30 14:37:42 +0200

    Added more comments in the Ingredient class so that the user understands exactly whats happening in this class

--- ingredients.cs
+++ ingredients.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 
 namespace ProgPart1
 {
-
-
     internal class Ingredient
-    { 
+    {
         // Properties for ingredient details
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string Unit { get; set; }
-        public int Calories { get; set; }
-        public string FoodGroup { get; set; }
+        public string Name { get; set; } // Name of the ingredient
+        public double Quantity { get; set; } // Quantity of the ingredient
+        public string Unit { get; set; } // Unit of measurement for the ingredient
+        public int Calories { get; set; } // Number of calories in the ingredient
+        public string FoodGroup { get; set; } // Food group the ingredient belongs to
 
         // Constructor to initialize an ingredient object
         public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
         {
-            Name = name;
-            Quantity = quantity;
-            Unit = unit;
-            Calories = calories;
-            FoodGroup = foodGroup;
+            Name = name; // Set the name of the ingredient
+            Quantity = quantity; // Set the quantity of the ingredient
+            Unit = unit; // Set the unit of measurement for the ingredient
+            Calories = calories; // Set the number of calories in the ingredient
+            FoodGroup = foodGroup; // Set the food group of the ingredient
         }
     }
 }
